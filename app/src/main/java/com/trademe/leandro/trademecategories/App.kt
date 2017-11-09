a4902c3566db94b178becb533746b73218520987
Leandro <gringoperalta@gmail.com>
2017-11-10 07:58:56 +1300

    Add App and Component.

---
+++ app/src/main/java/com/trademe/leandro/trademecategories/App.kt
@@ -0,0 +1,15 @@
+package com.trademe.leandro.trademecategories
+
+import android.app.Application
+
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+/**
+ * Created by Leandro on 10/11/2017.
+ */
+
+class App : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
+            DaggerAppComponent.builder().create(this)
+}
---
+++ app/src/main/java/com/trademe/leandro/trademecategories/AppComponent.kt
@@ -0,0 +1,22 @@
+package com.trademe.leandro.trademecategories
+
+import javax.inject.Singleton
+
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+/**
+ * Created by Leandro on 10/11/2017.
+ */
+@Singleton
+@Component(modules = arrayOf(
+        AndroidInjectionModule::class,
+        AndroidSupportInjectionModule::class
+))
+interface AppComponent : AndroidInjector<App> {
+
+    @Component.Builder
+    abstract class Builder : AndroidInjector.Builder<App>()
+}
