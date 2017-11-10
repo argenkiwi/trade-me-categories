bd40b400a4d536fa218280573bde4f2e58a69648
Leandro <gringoperalta@gmail.com>
2017-11-11 07:58:43 +1300

    Add oAuth signing to requests using provided consumer key and secret.

--- app/src/main/java/com/trademe/leandro/trademecategories/ServicesModule.kt
+++ app/src/main/java/com/trademe/leandro/trademecategories/ServicesModule.kt
@@ -9,6 +9,8 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer
+import se.akerfeldt.okhttp.signpost.SigningInterceptor
 import javax.inject.Singleton
 
 /**
@@ -26,13 +28,35 @@ object ServicesModule {
 
     @JvmStatic
     @Provides
-    @Singleton
-    fun gson() = GsonBuilder().create()
+    fun httpLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BASIC)
 
     @JvmStatic
     @Provides
-    fun httpLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor()
-            .setLevel(HttpLoggingInterceptor.Level.BASIC)
+    fun okHttpOAuthConsumer(): OkHttpOAuthConsumer = OkHttpOAuthConsumer(
+            BuildConfig.CONSUMER_KEY,
+            BuildConfig.CONSUMER_SECRET
+    )
+
+    @JvmStatic
+    @Provides
+    fun signingInterceptor(consumer: OkHttpOAuthConsumer) = SigningInterceptor(consumer)
+
+    @JvmStatic
+    @Provides
+    fun okHttpClient(
+            httpLoggingInterceptor: HttpLoggingInterceptor,
+            signingInterceptor: SigningInterceptor
+    ): OkHttpClient = OkHttpClient()
+            .newBuilder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(signingInterceptor)
+            .build()
+
+    @JvmStatic
+    @Provides
+    @Singleton
+    fun gson(): Gson = GsonBuilder().create()
 
     @JvmStatic
     @Provides
@@ -44,14 +68,5 @@ object ServicesModule {
             .client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build()
-
-    @JvmStatic
-    @Provides
-    fun okHttpClient(
-            interceptor: HttpLoggingInterceptor
-    ): OkHttpClient = OkHttpClient()
-            .newBuilder()
-            .addInterceptor(interceptor)
             .build()
 }
