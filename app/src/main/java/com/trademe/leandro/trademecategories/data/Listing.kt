29d95461222c56d1506e322f3b3b24fb1f9dbe2b
Leandro <gringoperalta@gmail.com>
2017-11-11 16:27:47 +1300

    Implement listing view and bind values.

--- app/src/main/java/com/trademe/leandro/trademecategories/data/Listing.kt
+++ app/src/main/java/com/trademe/leandro/trademecategories/data/Listing.kt
@@ -5,4 +5,8 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by Leandro on 11/11/2017.
  */
-data class Listing(@SerializedName("ListingId") val id: Int)
+data class Listing(
+        @SerializedName("ListingId") val id: Int,
+        @SerializedName("Title") val title: String,
+        @SerializedName("PictureHref") val pictureHref: String
+)
--- app/src/main/java/com/trademe/leandro/trademecategories/listings/ListingsAdapter.kt
+++ app/src/main/java/com/trademe/leandro/trademecategories/listings/ListingsAdapter.kt
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.trademe.leandro.trademecategories.R
 import com.trademe.leandro.trademecategories.data.Listing
 
@@ -14,7 +16,9 @@ class ListingsAdapter(
         private val listings: List<Listing>
 ) : RecyclerView.Adapter<ListingsAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        // TODO Bind data to view.
+        // TODO Load thumbnail.
+        holder?.titleView?.text = listings[position].title
+        holder?.idView?.text = "${listings[position].id}"
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
@@ -25,9 +29,9 @@ class ListingsAdapter(
 
     override fun getItemCount() = listings.size
 
-    class ViewHolder(
-            val listingView: View
-    ) : RecyclerView.ViewHolder(listingView) {
-
+    class ViewHolder(listingView: View) : RecyclerView.ViewHolder(listingView) {
+        val thumbnailView: ImageView = listingView.findViewById<ImageView>(R.id.thumbnail)
+        val titleView: TextView = listingView.findViewById<TextView>(R.id.title)
+        val idView: TextView = listingView.findViewById<TextView>(R.id.id)
     }
 }
