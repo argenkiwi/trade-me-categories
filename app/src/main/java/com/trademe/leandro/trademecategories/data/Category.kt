009c0547bbbd5ada2c4c1b604fc95ebab700b9a2
Leandro <gringoperalta@gmail.com>
2017-11-12 09:26:25 +1300

    Add category number to data class.

--- app/src/main/java/com/trademe/leandro/trademecategories/data/Category.kt
+++ app/src/main/java/com/trademe/leandro/trademecategories/data/Category.kt
@@ -6,6 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Created by Leandro on 10/11/2017.
  */
 data class Category(
+        @SerializedName("Number") val number: String,
         @SerializedName("Name") val name: String,
         @SerializedName("Subcategories") val subcategories: List<Category> = ArrayList()
 )
